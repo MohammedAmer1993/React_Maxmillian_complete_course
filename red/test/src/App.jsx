90eda04f3e7e96beee95c6b50fd54751ef72677b
Mohammed Amer <loard.amer.2010@gmail.com>
2025-01-13 02:30:07 +0200

    clean up

---
+++ red/test/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App({ name }) {
+  return (
+    <dialog open>
+      <h1>Mohammed amer</h1>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+}
+
+export default App;
---
+++ styling_tailwind_projStarter/src/components/Input.jsx
@@ -0,0 +1,18 @@
+export default function Input({ label, invalid, ...props }) {
+  let labelClass = "block mb-2 text-xs font-bold tracking-wide uppercase";
+  let inputClass = "w-full px-3 py2 leading-tight  border rounded shadow";
+
+  if (invalid) {
+    labelClass += " text-red-400";
+    inputClass += " text-red-500 bg-red-100 border-red-300";
+  } else {
+    labelClass += " text-stone-300";
+    inputClass += " bg-stone-300 text-gray-700";
+  }
+  return (
+    <p>
+      <label className={labelClass}>{label}</label>
+      <input className={inputClass} {...props} />
+    </p>
+  );
+}
