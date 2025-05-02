a90586162b93e8a8009403e559483df59c4e6f47
Mohammed Amer <loard.amer.2010@gmail.com>
2025-05-02 07:35:41 +0300

    got the token from backend and attached to each request

--- Authenticaion in reac Apps/frontend/src/components/AuthForm.js
+++ Authenticaion in reac Apps/frontend/src/components/AuthForm.js
@@ -1,32 +1,58 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
-
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const actionData = useActionData();
+  const [searchParams] = useSearchParams();
+  const { state } = useNavigation();
+  const isLogin = searchParams.get("mode") === "login" ? true : false;
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <h1>{isLogin ? "Welcome back" : "Create a new user"}</h1>
+        {actionData && actionData.errors && (
+          <ul>
+            {Object.values(actionData.errors).map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+        {actionData && actionData.message && <p>{actionData.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
         </p>
         <p>
-          <label htmlFor="image">Password</label>
+          <label htmlFor="password">Password</label>
           <input id="password" type="password" name="password" required />
         </p>
+        {!isLogin && (
+          <p>
+            <label htmlFor="confirm">Confirm Password</label>
+            <input id="confirm" type="password" name="confirm" />
+          </p>
+        )}
+        {!isLogin && (
+          <p>
+            <label htmlFor="username">Password</label>
+            <input id="username" type="password" name="username" />
+          </p>
+        )}
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
-            {isLogin ? 'Create new user' : 'Login'}
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "don't have an accout? Sign up" : "Login"}
+          </Link>
+          <button disabled={state === "submitting" ? true : false}>
+            {state === "submitting " ? "submitting..." : "save"}
           </button>
-          <button>Save</button>
         </div>
       </Form>
     </>
--- Authenticaion in reac Apps/frontend/src/pages/Authentication.js
+++ Authenticaion in reac Apps/frontend/src/pages/Authentication.js
@@ -1,7 +1,45 @@
-import AuthForm from '../components/AuthForm';
+import { redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
-function AuthenticationPage() {
+export default function AuthenticationPage() {
   return <AuthForm />;
 }
 
-export default AuthenticationPage;
+export async function authAction({ request }) {
+  const mode = new URL(request.url).searchParams.get("mode");
+  const data = await request.formData();
+  if (mode !== "login" && mode !== "signup") {
+    throw new Response(
+      JSON.stringify({
+        message: "Entered search parameters are not supported",
+      }),
+      { status: 422 }
+    );
+  }
+
+  const dataObj = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(dataObj),
+  });
+
+  if (response.status === 422 || response.status === 400) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw new Response(
+      JSON.stringify({ message: "Couldn't create new user" }),
+      { status: response.status }
+    );
+  }
+
+  const authData = await response.json();
+  const token = authData.token;
+  localStorage.setItem("token", token);
+  return redirect("/");
+}
