430227660ae6f363f99fb47d5beeafff68967a12
Mohammed Amer <loard.amer.2010@gmail.com>
2025-05-05 06:26:11 +0300

    started using useQuery of tanstack library it looks good

---
+++ tanstack query/src/components/Events/FindEventSection.jsx
@@ -0,0 +1,59 @@
+import { useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import ErrorBlock from "../UI/ErrorBlock";
+import EventItem from "./EventItem";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import { fetchEvents } from "../../util/util";
+
+export default function FindEventSection() {
+  const searchElement = useRef();
+  const [searchState, setSearchState] = useState("");
+  const { data, ispending, isError, error } = useQuery({
+    queryKey: ["events", { serchTerm: searchState }],
+    queryFn: ({ signal }) => fetchEvents({ searchTerm: searchState, signal }),
+  });
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSearchState(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+  if (ispending) {
+    content = <LoadingIndicator />;
+  }
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to find matched events"}
+      />
+    );
+  }
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  return (
+    <section className="content-section" id="all-events-section">
+      <header>
+        <h2>Find your next event!</h2>
+        <form onSubmit={handleSubmit} id="search-form">
+          <input
+            type="search"
+            placeholder="Search events"
+            ref={searchElement}
+          />
+          <button>Search</button>
+        </form>
+      </header>
+      {content}
+    </section>
+  );
+}
